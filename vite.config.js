b1eca70b5e3342911567b0a162fe23a98929dd9b
Alvaro Neira <alvaroneira98@gmail.com>
2024-04-28 18:03:22 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://neira21.github.io/Sunnyside-agency-landing-page-Neira21'
 })
